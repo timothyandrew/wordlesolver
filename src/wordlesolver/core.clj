12ef8d6271b8c714342a7f41c6143ed504ace911
Timothy Andrew <mail@timothyandrew.net>
2022-01-27 17:25:33 +0000

    Initial commit

---
+++ src/wordlesolver/core.clj
@@ -0,0 +1,35 @@
+(ns wordlesolver.core
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn cull-words [candidate words pattern]
+  (let [preds
+        (map-indexed
+         (fn [i letter]
+           (case letter
+             "Y" #(= (get candidate i) (get % i))
+             "M" #(and (not= (get candidate i) (get % i))
+                       (str/includes? % (str (get candidate i))))
+             "N" #(not (str/includes? % (str (get candidate i))))))
+         (-> pattern str/trim (str/split #"")))]
+    (filter (fn [word] (every? #(% word) preds)) words)))
+
+(defn main-loop [words]
+  (loop [words (shuffle words)]
+    (if (= 1 (count words))
+      (first words)
+      (let [candidate (first words)]
+        (println "Try word:" candidate)
+        (print "Enter pattern: ")
+        (flush)
+        (recur (cull-words candidate words (read-line)))))))
+
+(defn -main
+  []
+  (println "HELLo")
+  (let [words (-> "/Users/tim/wordlist"
+                  slurp
+                  (str/split #"\n"))
+        words (filter #(= (count %) 5) words)
+        result (main-loop words)]
+    (println "Result:" result)))
---
+++ test/wordlesolver/core_test.clj
@@ -0,0 +1,7 @@
+(ns wordlesolver.core-test
+  (:require [clojure.test :refer :all]
+            [wordlesolver.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
